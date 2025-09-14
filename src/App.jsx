e861d0b0fe41fc8d17b8a7c26392b435e9081946
Subhajit Ghosh <gamingsubha07@gmail.com>
2025-09-14 14:53:59 +0530

    Fix mobile responsiveness for App and Play pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import dice from './assets/dices.png'
-function App() { 
 
+function App() {
   return (
-    <div className="flex h-screen">
-    <div>
-      <img 
-        src={dice} 
-        className="h-screen w-240 mx-20" 
-        alt="Ludo Dice"
-      />
+    <div className="flex flex-col md:flex-row items-center justify-center h-screen p-4">
+
+      {/* Dice Image */}
+      <div className="mb-8 md:mb-0">
+        <img 
+          src={dice} 
+          className="w-48 sm:w-64 md:w-240 mx-auto"
+          alt="Ludo Dice"
+        />
+      </div>
+
+      {/* Text & Button */}
+      <div className="flex flex-col items-center gap-6 text-center">
+        <h1 className="text-4xl sm:text-5xl font-bold">DICE GAME</h1>
+
+        <NavLink to="/play">
+          <button className="bg-black text-white font-bold py-2 px-8 sm:px-14 rounded cursor-pointer hover:bg-white hover:text-black hover:border-2 hover:border-black transition duration-300 ease-in-out">
+            START
+          </button>
+        </NavLink>
+      </div>
+
     </div>
-    <div className="flex flex-col justify-center gap-8">
-      <h1 className="text-5xl font-bold ">DICE GAME</h1>
-      <NavLink to="/play">
-        <button className="bg-black ml-24 text-white font-bold py-2 px-14 rounded cursor-pointer hover:bg-white hover:text-black hover:border-2 hover:border-black transition duration-300 ease-in-out">
-          START
-        </button>
-      </NavLink>
-    </div>
-  </div>
-  
-  
   )
 }
 
